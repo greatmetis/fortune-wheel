90e423ce9df652980383342822664982697caa3e
Metis Teng <greatmetis@gmail.com>
2022-02-18 13:05:11 +1000

    Add: adding function for 2022 and reset

--- js/draw.js
+++ js/draw.js
@@ -5,58 +5,82 @@ const colors = {
   pink:"#FF00BA"
 };
 
+// FIXME: unable to redraw when selecting different year
 
 // init canvas setting
 const canvas = document.getElementById('wheel');
 const ctx = canvas.getContext('2d');
 const radius = 270; // wheel radius
-const PI = Math.PI;
 const PI2 = Math.PI*2;
-
 canvas.width = radius * 2;
 canvas.height = radius * 2;
 
 // draw sectors
-ctx.fillPie = function(x,y,r,start){
-  let angle = PI / 4;
+ctx.fillPie = function(x,y,r,start,qty){
+  let angle = PI2 / qty;
   this.beginPath()
   ctx.lineTo(radius, radius)
   this.arc(x,y,r,start*angle,(start+1)*angle)
   ctx.lineTo(radius,radius)
   this.fill()
+  this.closePath()
 };
 
 const iconGroup = document.querySelector(".icon-group");
-
-function insertContent(data,qty){
+// iconGroup.style.transform= "rotate(90deg)"
+function insertContent(data,qty,year){
   let rotate = 360 / qty
-  data.forEach((item,index)=>{
-    let html = `
-    <i class="${item.icon}"></i>
-    <h5>${item.text}</h5>
-    <span>${item.num}</span>
-    `
-    let newContent = document.createElement("li")
-    newContent.innerHTML = html
-    iconGroup.append(newContent)
-    newContent.style.transform = `rotate(${index*rotate}deg)` 
-  })
+  iconGroup.innerHTML = ''
+    data.forEach((item,index)=>{
+      let html = ''
+      if(year == 2021){
+        html = `
+        <i class="${item.icon}"></i>
+        <h5>${item.label}</h5>
+        <span>${item.num}</span>
+        `
+      }else{
+        html = `
+        <h5 class="new">${item.label}</h5>
+        <span >${item.num}</span>`
+      }
+      let newContent = document.createElement("li")
+      newContent.innerHTML = html
+      iconGroup.append(newContent)
+      newContent.style.transform = `rotate(${index*rotate}deg)` 
+    })
 };
 
-export default function draw(data,qty){
-  for(let i = 0 ; i<=8 ; i+=2){
+function drawButton(){
+  ctx.beginPath();
+  ctx.fillStyle = colors.blueDark;
+  ctx.arc(radius,radius,55,0,PI2);
+  ctx.closePath();
+  ctx.fill();
+}
+
+export function drawSectors(qty){
+  for(let i = 0 ; i<=qty ; i+=2){
     ctx.fillStyle = colors.pinkLight;
-    ctx.fillPie(radius,radius,radius,i);
+    ctx.fillPie(radius,radius,radius,i,qty);
   }
-  for(let i = 1 ; i<=8 ; i+=2){
+  for(let i = 1 ; i<=qty ; i+=2){
     ctx.fillStyle = colors.blue;
-    ctx.fillPie(radius,radius,radius,i);
+    ctx.fillPie(radius,radius,radius,i,qty);
   }
+}
+
+export function draw(data,qty,year){
+  drawSectors(qty)
+  
     // draw press button
-    ctx.beginPath()
-    ctx.fillStyle = colors.blueDark;
-    ctx.translate(radius,radius);
-    ctx.arc(0,0,55,0,PI2);
-    ctx.fill();
-    insertContent(data,qty)
+    drawButton();
+    insertContent(data,qty,year);
 };
+
+export function drawSelected(i,qty){
+  ctx.beginPath();
+  ctx.fillStyle = colors.pink;
+  ctx.fillPie(radius,radius,radius,i,qty);
+  drawButton();
+}
--- js/main.js
+++ js/main.js
@@ -2,10 +2,15 @@
 
 // <!-- 員工150人, 獎品20個  -->
 // <!-- 放上編號就好 -->
-import draw from "./draw.js"
+import {draw,drawSelected,drawSectors} from "./draw.js"
 let data = [];
 let game = null;
+const playTimes = {
+  "2021":20,
+  "2022":150
+};
 
+const wheel = document.querySelector("#wheel")
 const pointer = document.querySelector(".pointer");
 const pressBtn = document.querySelector(".pointer span");
 const banner = document.querySelector(".banner");
@@ -13,27 +18,38 @@ const infoText = document.querySelector(".info-text");
 const selectedPresent = document.querySelector("#selected-present");
 const btn_2021 = document.querySelector("#btn-2021");
 const btn_2022 = document.querySelector("#btn-2022");
+const btn_reset = document.querySelector("#btn-reset");
+
 async function fetchData(url){
   let resp = await fetch(url)
   data = await resp.json()
 };
-
-async function init(val){
-  await fetchData(val);
-  draw(data,data.length);
-  game = new Game(data,20,data.length)
+function reset(){
+  banner.style.display = 'none'
+  pointer.style.transform = 'rotate(0deg)'
 };
-init("./data_2021.json");
+
+async function init(val,year){
+  await fetchData(val);
+  draw(data,data.length,year);
+  game = new Game(year,data,playTimes[year],data.length)
+};
+init("./data_2021.json",2021);
+
 
 // Event Listners
 pressBtn.addEventListener("click",()=>{
-  console.log(data)
   game.spinWheel()
   game.isTurning = true // prevent clicking before it stop
-  
+  btn_2021.disabled = true
+  btn_2022.disabled = true
+  btn_reset.disabled = true
 });
 
 pointer.addEventListener("transitionend",()=>{
+  btn_2021.disabled = false
+  btn_2022.disabled = false
+  btn_reset.disabled = false
   pointer.style.transition = 'none';
   let actualDeg = (game.deg-3600);
   game.deg = actualDeg;
@@ -45,27 +61,42 @@ pointer.addEventListener("transitionend",()=>{
     game.isEnd = true
   }
   game.showResult()
+
 });
 
 btn_2021.addEventListener("click",()=>{
-  console.log('2021')
-  init("./data_2021.json");
+  reset()
+  pointer.style.transform = 'rotate(0deg)'
+  init(`./data_${btn_2021.value}.json`,btn_2021.value);
+  wheel.classList.remove("new")
 
 })
 btn_2022.addEventListener("click",()=>{
-  console.log('2022')
-  // init("../data_2022.json");
+  reset()
+  pointer.style.transform = 'rotate(0deg)'
+  init(`./data_${btn_2022.value}.json`,btn_2022.value);
+  wheel.classList.add("new")
+
 })
 
+btn_reset.addEventListener("click",()=>{
+  reset()
+  let currentYear = game.year
+  currentYear==2021 ? btn_2021.click() : btn_2022.click()
+})
 
 // Init Game object
-const Game = function(dataBase,times,presentQty){
+const Game = function(year,dataBase,times,presentQty){
+  this.year = year
   this.pool = [...dataBase]
   this.playTimes = times
   this.presentQty = presentQty // define the num of sectors
   this.isTurning = false
   this.isEnd = false
-  this.currentResult = ''
+  this.currentResult = {
+    index:null,
+    label:null
+  }
 }
 
 // spin the wheel
@@ -73,40 +104,39 @@ Game.prototype.spinWheel = function(){
   if(this.isEnd || this.isTurning) return    // no spinning
 
   this.getPresent()
-  let index = data.findIndex(x=>x.text===`${this.currentResult}`)
+  let index = data.findIndex(x=>x.label===`${this.currentResult.label}`)
   this.deg = index*360 / this.presentQty+3600
-
   banner.style.display = 'none'
   pointer.style.transform = `rotate(${this.deg}deg)`
   pointer.style.transition = 'all 3s ease-out'
-
 };
 
 // re-rendering num in present pool
 Game.prototype.renderNum = function(){
   document.querySelectorAll(".icon-group span").forEach((item,index)=>{    
     item.innerText = data[index].num
+
   })
 };
 
 Game.prototype.showResult = function(){
   banner.style.display = 'flex'
-  selectedPresent.innerText = this.currentResult
+  selectedPresent.innerText = this.currentResult.label
   if(this.isEnd){
     infoText.innerText = 'The End'
     return
   }
-  infoText.innerText = 'Congrats!'
-}
+  infoText.innerText = 'well done!'
+};
 
 Game.prototype.getPresent = function(){
-  let num = Math.floor(Math.random()*this.pool.length)
-  this.currentResult = this.pool[num].text
+  let index = Math.floor(Math.random()*this.pool.length)
+  this.currentResult.label = this.pool[index].label
+  this.currentResult.index = index
 
   // delete present fram present pool if the num is 0
-  this.pool[num].num --
-
-  if(this.pool[num].num === 0) {
-    this.pool.splice(num,1)
+  this.pool[index].num --
+  if(this.pool[index].num === 0) {
+    this.pool.splice(index,1)
   }
-}
+};
